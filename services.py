99361217284e4c3ac5224af858dc77a5822c4e55
Mikhail Opryshko <mikhail.opryshko@ccsteam.ru>
2025-08-07 14:51:29 +0300

    test webhook

--- services.py
+++ services.py
@@ -5,8 +5,8 @@ from typing import IO
 
 
 async def get_id_nickname(name):
-    name = str(name)
-    name = name.strip()
+    # name = str(name)
+    name = str(name).strip()
     if len(name.split()) > 1:
         return f"Пользователь с ником {name} не найден"
     data = None
